bc880693eb9fd06baf26f8b5606b9f782a2918d9
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2020-09-01 07:55:32 +0100

    Add files via upload

---
+++ publishermodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace LinqToSQLMvcApplication.Models
+{
+    public class publishermodel
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Year { get; set; }
+    }
+}

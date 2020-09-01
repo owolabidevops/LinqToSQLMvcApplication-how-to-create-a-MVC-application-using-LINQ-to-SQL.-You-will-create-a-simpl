697b6547de430226bb13443d5b7b5f08219710c1
Owolabi <48913682+owolabidevops@users.noreply.github.com>
2020-09-01 07:54:39 +0100

    Add files via upload

---
+++ bookmodel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinqToSQLMvcApplication.Models
+{
+    public class bookmodel
+    {
+        public bookmodel()
+        {
+            publishers = new List<SelectListItem>();
+        }
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Author { get; set; }
+        public String Year { get; set; }
+        public String Price { get; set; }
+        [DisplayName("Publisher")]
+        public int PublisherId { get; set; }
+        public String PublisherName { get; set; }
+        public IEnumerable<SelectListItem> publishers { get; set; }
+
+    
+
+    }
+}
